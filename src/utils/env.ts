77337a307832fa3a9076b3da0c0c260cc31721f2
Suman Debnath <54632628+SUMExXx@users.noreply.github.com>
2024-11-20 13:17:09 +0530

    added linux compatibility

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,3 +1,7 @@
+import os from "os";
+
+const isWindows = os.platform() === "win32";
+
 interface ENV{
     API_BASE_URL: string,
     ROOT_PATH: string,
@@ -8,8 +12,8 @@ interface ENV{
 
 export const env:ENV = {
     API_BASE_URL: "http://localhost:8080",
-    ROOT_PATH: "D://ShareHub/",
-    STORAGE_PATH: "D://ShareHub/storage/",
+    ROOT_PATH: isWindows? "D://ShareHub/" : "~/ShareHub",
+    STORAGE_PATH: isWindows? "D://ShareHub/storage/" : "~/ShareHub/storage",
     // USER_ID: "dbfebdf7-17e2-458a-87cd-6e1e1fb5cb6d"
     USER_ID: "26e00960-c938-4861-a2ff-b4403158b689",
     EXPRESS_PORT: 8000
